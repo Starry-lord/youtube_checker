eba3e2d4c8a84ecd4146e4268afb5444c0e7be69
Starry-lord <66038734+Starry-lord@users.noreply.github.com>
2022-02-04 22:30:45 +0100

    Update youtubechecker.py

--- youtubechecker.py
+++ youtubechecker.py
@@ -29,24 +29,30 @@ except FileNotFoundError:
     wordlist.close()
     print("list downloaded")
 finally:
-    print("list available")
+    print("wordlist check done :3")
 
 print("""
 Usage: python3 youtubechecker.py
 
 Description: Run the title or description text of your Youtube video through the list to better anticipate demonetization.
 
-Note: to update the list simply remove the wordlist.csv and wordlist_clean and rerun.
+Note: to update the list use "rm wordlist*" and rerun.
 """)
 
-with open("wordlist.csv", "r", newline="") as o :
-    with open("wordlist_clean", "w") as f:
-        reader = csv.reader(o, delimiter=",")
-        with contextlib.redirect_stdout(f):
-            for row in reader:
-                print(row[0])
+try:
+    f = open("wordlist_clean","r")
+    f.close()
+except FileNotFoundError:
+    with open("wordlist.csv", "r", newline="") as o :
+        with open("wordlist_clean", "w") as f:
+            reader = csv.reader(o, delimiter=",")
+            with contextlib.redirect_stdout(f):
+                for row in reader:
+                    print(row[0])
 
-print('wordlist_clean created successfully!')
+    #print('wordlist_clean created successfully')
+finally:
+    print("wordlist cleanup check done :3")
 
 # input
 
@@ -57,11 +63,17 @@ i = inpuT.split(' ')
 
 badword_list = open('wordlist_clean').read().splitlines()
 
+flag = 0
 for word in i:
     if (word in badword_list) == True:
         print(word)
-        print("Change your words!")
-    else:
-        print("$$$ Yay, Youtube should pay! $$$")
-        print("Such polite! :3")
-        break
+        #print("Change your words!")
+        flag = 1
+    elif word not in badword_list:
+        flag = flag
+
+if flag == 0 :
+    print("$$$ Yay, Youtube should pay! $$$")
+    print("Such polite! :3")
+else :
+    print("Change your words!")
